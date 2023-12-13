c6590129117a0e18af4599dd77ae056ab1d1f1af
Salim Kachemela <sakachemela@gmail.com>
2023-12-13 22:58:14 +0300

    Add some minor configurations for the terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,13 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+local mux = wezterm.mux
+
+-- Maximize the window right away
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
 local config = {}
 
 -- Use config builder object if possible
@@ -14,6 +23,20 @@ config.font = wezterm.font_with_fallback({
 	{ family = "Operator Mono Lig", scale = 1.5, weight = "Light" },
 	-- { family = "FuraCode Nerd Font", scale = 1.4, weight = "Light" },
 })
+
+config.check_for_updates = true
+
+-- Disable all noises
+config.audible_bell = "Disabled"
+
+config.keys = {
+	{
+		key = "C",
+		mods = "CTRL",
+		action = act.CopyTo("ClipboardAndPrimarySelection"),
+	},
+}
+
 -- config.window_background_image = "/home/profkache/Pictures/term_wallpapers/pxfuel.jpg"
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
@@ -26,5 +49,10 @@ config.window_padding = {
 config.scrollback_lines = 3500
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+
+-- Open in full screen
+config.initial_rows = 50
+config.initial_cols = 200
 
 return config
