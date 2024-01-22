3b7322bb9091747927e6af29f5d288e690f76937
Salim Kachemela <sakachemela@gmail.com>
2024-01-22 03:59:46 +0300

    Add configuration to ensure auto installation for any lsp

--- .config/nvim/lua/mason-config/init.lua
+++ .config/nvim/lua/mason-config/init.lua
@@ -1,6 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "tsserver", "tailwindcss", "pyright", "pylsp" },
+	automatic_installation = true,
 })
 
 -- After setting up mason-lspconfig you may set up servers via lspconfig
