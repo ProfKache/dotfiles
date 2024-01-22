c4e1ffa6410e5d5664786a4b5e71b7f486e9400e
Salim Kachemela <sakachemela@gmail.com>
2024-01-22 05:36:07 +0300

    Fix: jdtls configuration for java development

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,157 +1,200 @@
 local home = os.getenv("HOME")
+local jdtls = require("jdtls")
 
-local jdtls_dir = vim.fn.stdpath(home .. "/.local/share/nvim/mason/packages/jdtls")
-local plugins_dir = jdtls_dir .. "/plugins"
-local path_to_jar = plugins_dir .. "org.eclipse.equinox.launcher_*.jar"
-
--- Determine OS
-if vim.fn.has("mac") == 1 then
-	WORKSPACE_PATH = home .. "/workspace/"
-	CONFIG = "mac"
-elseif vim.fn.has("unix") == 1 then
-	WORKSPACE_PATH = home .. "/workspace/"
-	CONFIG = "linux"
-else
-	print("Unsupported system")
-end
-
--- Find the root of the project
+-- File types that signify a Java project's root directory. This will be
+-- used by eclipse to determine what constitutes a workspace
+-- local root_markers = { "gradlew", "mvnw", ".git" }
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
-if root_dir == "" then
-	return
+
+-- eclipse.jdt.ls stores project specific data within a folder. If you are working
+-- with multiple different projects, each project must use a dedicated data directory.
+-- This variable is used to configure eclipse to use the directory name of the
+-- current project found using the root_marker as the folder for project specific data.
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+-- Helper function for creating keymaps
+function nnoremap(rhs, lhs, bufopts, desc)
+	bufopts.desc = desc
+	vim.keymap.set("n", rhs, lhs, bufopts)
 end
 
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+-- The on_attach function is used to set key maps after the language server
+-- attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Regular Neovim LSP client keymappings
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	nnoremap("gD", vim.lsp.buf.declaration, bufopts, "Go to declaration")
+	nnoremap("gd", vim.lsp.buf.definition, bufopts, "Go to definition")
+	nnoremap("gi", vim.lsp.buf.implementation, bufopts, "Go to implementation")
+	nnoremap("K", vim.lsp.buf.hover, bufopts, "Hover text")
+	nnoremap("<C-k>", vim.lsp.buf.signature_help, bufopts, "Show signature")
+	nnoremap("<space>wa", vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
+	nnoremap("<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
+	nnoremap("<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts, "List workspace folders")
+	nnoremap("<space>D", vim.lsp.buf.type_definition, bufopts, "Go to type definition")
+	nnoremap("<space>rn", vim.lsp.buf.rename, bufopts, "Rename")
+	nnoremap("<space>ca", vim.lsp.buf.code_action, bufopts, "Code actions")
+	vim.keymap.set(
+		"v",
+		"<space>ca",
+		"<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+		{ noremap = true, silent = true, buffer = bufnr, desc = "Code actions" }
+	)
+	nnoremap("<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts, "Format file")
 
-local workspace_dir = WORKSPACE_PATH .. project_name
+	-- Java extensions provided by jdtls
+	nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+	nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+	nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+	vim.keymap.set(
+		"v",
+		"<space>em",
+		[[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+		{ noremap = true, silent = true, buffer = bufnr, desc = "Extract method" }
+	)
+end
 
 local config = {
-	-- The command that starts the language server
+	flags = {
+		debounce_text_changes = 80,
+	},
+	on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
+	root_dir = root_dir, -- Set the root directory to our found root_marker
+	-- Here you can configure eclipse.jdt.ls specific settings
+	-- These are defined by the eclipse.jdt.ls project and will be passed to eclipse when starting.
+	-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+	-- for a list of options
+	settings = {
+		java = {
+			format = {
+				settings = {
+					-- Use Google Java style guidelines for formatting
+					-- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+					-- and place it in the ~/.local/share/eclipse directory
+					url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+					profile = "GoogleStyle",
+				},
+			},
+			signatureHelp = { enabled = true },
+			contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
+			-- Specify any completion options
+			completion = {
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+					"org.mockito.Mockito.*",
+				},
+				filteredTypes = {
+					"com.sun.*",
+					"io.micrometer.shaded.*",
+					"java.awt.*",
+					"jdk.*",
+					"sun.*",
+				},
+			},
+			-- Specify any options for organizing imports
+			sources = {
+				organizeImports = {
+					starThreshold = 9999,
+					staticStarThreshold = 9999,
+				},
+			},
+			-- How code generation should act
+			codeGeneration = {
+				toString = {
+					template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+				},
+				hashCodeEquals = {
+					useJava7Objects = true,
+				},
+				useBlocks = true,
+			},
+			-- If you are developing in projects with different Java versions, you need
+			-- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
+			-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+			-- And search for `interface RuntimeOption`
+			-- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+			configuration = {
+				runtimes = {
+					{
+						name = "JavaSE-17",
+						path = home .. "/.asdf/installs/java/openjdk-17.0.2",
+					},
+					{
+						name = "JavaSE-14",
+						path = home .. "/.asdf/installs/java/openjdk-14.0.2",
+					},
+					{
+						name = "JavaSE-13",
+						path = home .. "/.asdf/installs/java/openjdk-13.0.2",
+					},
+					{
+						name = "JavaSE-11",
+						path = home .. "/.asdf/installs/java/openjdk-11.0.2",
+					},
+					{
+						name = "JavaSE-10",
+						path = home .. "/.asdf/installs/java/openjdk-10.0.2",
+					},
+					{
+						name = "JavaSE-1.8",
+						path = home .. "/.asdf/installs/java/adoptopenjdk-8.0.382+5",
+					},
+				},
+			},
+		},
+	},
+	-- cmd is the command that starts the language server. Whatever is placed
+	-- here is what is passed to the command line to execute jdtls.
+	-- Note that eclipse.jdt.ls must be started with a Java version of 17 or higher
 	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+	-- for the full list of options
 	cmd = {
-		-- 💀
-		home .. ".asdf/installs/java/openjdk-17.0.2/bin/java", -- or '/path/to/java17_or_newer/bin/java'
-		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
+		"java", -- or home .. "/.asdf/installs/java/openjdk-17.0.2/bin/java",
 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
 		"-Dosgi.bundles.defaultStartLevel=4",
 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
 		"-Dlog.protocol=true",
 		"-Dlog.level=ALL",
-		"-javaagent:" .. home .. jdtls_dir .. "/lombok.jar",
-		"-Xms1g",
+		"-Xmx4g",
 		"--add-modules=ALL-SYSTEM",
 		"--add-opens",
 		"java.base/java.util=ALL-UNNAMED",
 		"--add-opens",
-		"java.base/java.lang=ALL-UNNAMED", -- 💀
+		"java.base/java.lang=ALL-UNNAMED",
+		-- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
+		"-javaagent:"
+			.. home
+			.. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		-- .. "/.local/share/eclipse/lombok.jar",
+
+		-- The jar file is located where jdtls was installed. This will need to be updated
+		-- to the location where you installed jdtls
 		"-jar",
-		path_to_jar,
-		-- Must point to the                                                     Change this to
-		-- eclipse.jdt.ls installation                                           the actual version
+		vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		-- vim.fn.glob("/opt/homebrew/Cellar/jdtls/1.18.0/libexec/plugins/org.eclipse.equinox.launcher_*.jar"),
 
-		-- 💀
+		-- The configuration for jdtls is also placed where jdtls was installed. This will
+		-- need to be updated depending on your environment
 		"-configuration",
-		home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. CONFIG,
-		-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-		-- Must point to the                      Change to one of `linux`, `win` or `mac`
-		-- eclipse.jdt.ls installation            Depending on your system.
+		home .. "/.local/share/nvim/mason/bin/jdtls/config_linux",
+		-- "/opt/homebrew/Cellar/jdtls/1.18.0/libexec/config_mac",
 
-		-- 💀
-		-- See `data directory configuration` section in the README
+		-- Use the workspace_folder defined above to store data for this project
 		"-data",
-		workspace_dir,
-	},
-
-	root_dir = root_dir,
-
-	settings = {
-		["java.format.settings.url"] = home .. "/.config/nvim/formatters/eclipse-java-google-style.xml",
-		["java.format.settings.profile"] = "GoogleStyle",
-		java = {
-			-- jdt = {
-			--   ls = {
-			--     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
-			--   }
-			-- },
-			eclipse = { downloadSources = true },
-			maven = { downloadSources = true },
-			implementationsCodeLens = { enabled = true },
-			referencesCodeLens = { enabled = true },
-			references = { includeDecompiledSources = true },
-			format = {
-				enabled = true,
-				-- settings = { url = home .. "/.config/nvim/formatters/java-google-formatter.xml" },
-			},
-		},
-		signatureHelp = { enabled = true },
-		completion = {
-			favoriteStaticMembers = {
-				"org.hamcrest.MatcherAssert.assertThat",
-				"org.hamcrest.Matchers.*",
-				"org.hamcrest.CoreMatchers.*",
-				"org.junit.jupiter.api.Assertions.*",
-				"java.util.Objects.requireNonNull",
-				"java.util.Objects.requireNonNullElse",
-				"org.mockito.Mockito.*",
-			},
-		},
-		contentProvider = { preferred = "fernflower" },
-		-- extendedClientCapabilities = extendedClientCapabilities,
-		sources = {
-			organizeImports = {
-				starThreshold = 9999,
-				staticStarThreshold = 9999,
-			},
-		},
-		codeGeneration = {
-			toString = {
-				template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-			},
-			useBlocks = true,
-		},
-		configuration = {
-			runtime = {
-				{
-					name = "JavaSE-17",
-					path = home .. "/.asdf/installs/java/openjdk-17.0.2",
-				},
-				{
-					name = "JavaSE-14",
-					path = home .. "/.asdf/installs/java/openjdk-17.0.2",
-				},
-				{
-					name = "JavaSE-13",
-					path = home .. "/.asdf/installs/java/openjdk-13.0.2",
-				},
-				{
-					name = "JavaSE-11",
-					path = home .. "/.asdf/installs/java/openjdk-11.0.2",
-				},
-				{
-					name = "JavaSE-10",
-					path = home .. "/.asdf/installs/java/openjdk-10.0.2",
-				},
-				{
-					name = "JavaSE-1.8",
-					path = home .. "/.asdf/installs/java/adoptopenjdk-8.0.382+5",
-				},
-			},
-		},
-		flags = {
-			allow_incremental_sync = true,
-		},
-		init_options = {
-			bundles = {},
-		},
+		workspace_folder,
 	},
 }
 
--- local config = {
--- 	cmd = { "/home/profkache/.local/share/nvim/mason/bin/jdtls" },
--- 	root_dir = vim.fs.dirname(
--- 		vim.fs.find({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }, { upward = true })[1]
--- 	),
--- }
-
-require("jdtls").start_or_attach(config)
+-- Finally, start jdtls. This will run the language server using the configuration we specified,
+-- setup the keymappings, and attach the LSP client to the current buffer
+jdtls.start_or_attach(config)
