f72695dc006a2d3705fbb7a4a418f9bc910d7c83
Salim Kachemela <sakachemela@gmail.com>
2024-01-31 08:05:55 +0300

    Enable full-screen on startup of wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,15 +4,16 @@ local mux = wezterm.mux
 
 -- Maximize the window right away
 wezterm.on("gui-startup", function(cmd)
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local _, _, window = mux.spawn_window(cmd or {})
+	-- window:gui_window():maximize()  -- this works for other operating systems
+	window:gui_window():toggle_fullscreen() -- works for mac os
 end)
 
 local config = {}
 
 -- Use config builder object if possible
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Settings
@@ -20,8 +21,8 @@ end
 config.color_scheme = "Tokyo Night Storm"
 config.line_height = 1.25
 config.font = wezterm.font_with_fallback({
-  { family = "Operator Mono Lig", scale = 1.79, weight = "Light" },
-  -- { family = "FuraCode Nerd Font", scale = 1.4, weight = "Light" },
+	{ family = "Operator Mono Lig", scale = 1.79, weight = "Light" },
+	-- { family = "FuraCode Nerd Font", scale = 1.4, weight = "Light" },
 })
 
 config.check_for_updates = true
@@ -30,21 +31,21 @@ config.check_for_updates = true
 config.audible_bell = "Disabled"
 
 config.keys = {
-  {
-    key = "C",
-    mods = "CTRL",
-    action = act.CopyTo("ClipboardAndPrimarySelection"),
-  },
+	{
+		key = "C",
+		mods = "CTRL",
+		action = act.CopyTo("ClipboardAndPrimarySelection"),
+	},
 }
 
 -- config.window_background_image = "/home/profkache/Pictures/term_wallpapers/pxfuel.jpg"
 config.window_background_opacity = 1
 config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 config.scrollback_lines = 3500
 config.tab_bar_at_bottom = true
