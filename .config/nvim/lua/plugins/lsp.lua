91ffaa89eef1bacdf5002e0c1c8bbd38b74596c6
Salim Kachemela <sakachemela@gmail.com>
2024-06-28 10:14:18 +0300

    Add json schema plugin configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,8 @@ return {
 		-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
 		-- used for completion, annotations and signatures of Neovim apis
 		{ "folke/neodev.nvim", opts = {} },
+		-- Schema information
+		"b0o/SchemaStore.nvim",
 	},
 	config = function()
 		--  This function gets run when an LSP attaches to a particular buffer.
