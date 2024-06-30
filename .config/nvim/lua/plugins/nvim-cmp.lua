4670d6cc32372e62e92a7387ca0cae4b5e72cf7f
Salim Kachemela <sakachemela@gmail.com>
2024-07-01 00:04:01 +0300

    Add pictograms configuration upon completion

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -39,6 +39,7 @@ return {
 		-- See `:help cmp`
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 		luasnip.config.setup({})
 
 		cmp.setup({
@@ -105,6 +106,24 @@ return {
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "path" },
+			},
+			-- enable icons beside functions and variables
+			formatting = {
+				expandable_indicator = true,
+				fields = { "kind", "abbr", "menu" },
+				format = lspkind.cmp_format({
+					mode = "symbol_text", -- show only symbol annotations ('text'|'text_symbol'|'symbol_text', 'symbol')
+					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+					-- The function below will be called before any actual modifications from lspkind
+					-- so that you can provide more controls on popup customization.
+					before = function(entry, vim_item)
+						-- Customize vim_item here if needed
+						return vim_item
+					end,
+				}),
 			},
 		})
 	end,
