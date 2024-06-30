5dc21a6c9142162d62fec4118fc2e379b935f412
Salim Kachemela <sakachemela@gmail.com>
2024-06-30 19:22:08 +0300

    Add more configurations for catppuccin theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-	-- "navarasu/onedark.nvim",
+	"navarasu/onedark.nvim",
 	-- config = function()
 	-- 	require("onedark").setup({
 	-- 		style = "deep", -- deep, dark, darker, cool, warm, warmer
@@ -15,18 +15,41 @@ return {
 	-- 	require("onedark").load()
 	-- end,
 
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent = true,
-			styles = {
-				comments = { "italic" },
-				keywords = { "bold" },
-				functions = { "italic" },
-			},
-		})
-		vim.cmd.colorscheme("catppuccin")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				transparent = true,
+				styles = {
+					comments = { "italic" },
+					keywords = { "bold" },
+					functions = { "italic" },
+					conditionals = { "italic" },
+					loops = { "italic" },
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
