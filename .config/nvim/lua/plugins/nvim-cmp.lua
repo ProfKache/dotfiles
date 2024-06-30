56fc464a5ef8655ab4e3458d1d2520023c67b69d
Salim Kachemela <sakachemela@gmail.com>
2024-07-01 00:17:57 +0300

    Use symbols for pictograms

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -112,7 +112,7 @@ return {
 				expandable_indicator = true,
 				fields = { "kind", "abbr", "menu" },
 				format = lspkind.cmp_format({
-					mode = "symbol_text", -- show only symbol annotations ('text'|'text_symbol'|'symbol_text', 'symbol')
+					mode = "symbol", -- show only symbol annotations ('text'|'text_symbol'|'symbol_text', 'symbol')
 					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
