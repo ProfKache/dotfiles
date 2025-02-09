d3b04620cb9825fba1edf609717aeb687257b793
Salim Kachemela <sakachemela@gmail.com>
2025-02-09 22:57:41 +0300

    Change to a different lsp for elixir

--- .config/nvim/ftplugin/elixir.lua
+++ .config/nvim/ftplugin/elixir.lua
@@ -1,11 +1,15 @@
-require("lspconfig")["nextls"].setup({
-	cmd = { "nextls", "--stdio" },
-	init_options = {
-		extensions = {
-			credo = { enable = true },
-		},
-		experimental = {
-			completions = { enable = true },
-		},
-	},
+-- require("lspconfig")["nextls"].setup({
+-- 	cmd = { "nextls", "--stdio" },
+-- 	init_options = {
+-- 		extensions = {
+-- 			credo = { enable = true },
+-- 		},
+-- 		experimental = {
+-- 			completions = { enable = true },
+-- 		},
+-- 	},
+-- })
+
+require("lspconfig").elixirls.setup({
+	cmd = { vim.fn.stdpath("data") .. "/mason/bin/elixir-ls" },
 })
