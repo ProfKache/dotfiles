6ffd7f078f5bd2cb42cf315e5f419ed2df92c15c
Salim Kachemela <sakachemela@gmail.com>
2025-06-24 00:03:31 +0300

    Add plugin for running code

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,4 +1,12 @@
 return {
 	"stevearc/overseer.nvim",
-	opts = {},
+	cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+	opts = {
+		task_list = {
+			direction = "top",
+			min_height = 20,
+			max_height = 20,
+			default_detail = 1,
+		},
+	},
 }
