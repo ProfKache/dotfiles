489c7fb63734470cbcdbef598e2f05c80af9a51f
Salim Kachemela <sakachemela@gmail.com>
2025-07-11 16:06:06 +0300

    Enable wezterm clipboard for tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ wezterm.on("gui-startup", function(cmd)
 	local _, _, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize() -- this works for other operating systems
 	-- window:gui_window():toggle_fullscreen() -- works for mac os
+	-- window:copy_to_clipboard("Clipboard")
 end)
 
 local config = {}
