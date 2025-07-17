b89d4c5d5f9dd27185701004c73401fe4ea85154
Salim Kachemela <sakachemela@gmail.com>
2025-07-17 10:10:15 +0300

    Add modify symbol-usage to enable custom settings

--- .config/nvim/lua/plugins/symbol-usage.lua
+++ .config/nvim/lua/plugins/symbol-usage.lua
@@ -2,6 +2,6 @@ return {
 	"Wansmer/symbol-usage.nvim",
 	event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
 	config = function()
-		require("symbol-usage").setup()
+		require("symbol-usage").setup({})
 	end,
 }
