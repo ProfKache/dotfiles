12b1eb681d83c610f663d639fa4063ba5d3bb43f
Salim Kachemela <sakachemela@gmail.com>
2025-09-30 19:13:30 +0300

    Add configuration for including css files

--- .config/nvim/lua/plugins/nvim-html-css.lua
+++ .config/nvim/lua/plugins/nvim-html-css.lua
@@ -30,10 +30,6 @@ return {
 		documentation = {
 			auto_show = true,
 		},
-		style_sheets = {
-			"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-			"https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.3/css/bulma.min.css",
-			"./index.css", -- `./` refers to the current working directory.
-		},
+		vim.fn.globpath(vim.fn.getcwd(), "**/*.{css,scss,pcss,tailwind.css}", true, true), -- 🔥 expand all CSS files
 	},
 }
