dc38ee6dff43e06ba24ac8d013104255feefccfd
Salim Kachemela <sakachemela@gmail.com>
2025-10-13 10:29:46 +0300

    Update to use new configurations in elixirls

--- .config/nvim/ftplugin/elixir.lua
+++ .config/nvim/ftplugin/elixir.lua
@@ -1,16 +1,15 @@
--- require("lspconfig")["nextls"].setup({
--- 	cmd = { "nextls", "--stdio" },
--- 	init_options = {
--- 		extensions = {
--- 			credo = { enable = true },
--- 		},
--- 		experimental = {
--- 			completions = { enable = true },
+-- require("lspconfig").elixirls.setup({
+-- 	cmd = { vim.fn.stdpath("data") .. "/mason/bin/elixir-ls" },
+-- 	settings = {
+-- 		elixirLS = {
+-- 			dialyzerEnabled = true,
+-- 			fetchDeps = false,
 -- 		},
 -- 	},
 -- })
 
-require("lspconfig").elixirls.setup({
+-- New syntax for elixir-ls
+vim.lsp.config("elixirls", {
 	cmd = { vim.fn.stdpath("data") .. "/mason/bin/elixir-ls" },
 	settings = {
 		elixirLS = {
