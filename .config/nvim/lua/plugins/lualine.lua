00304850a6da9f120a709e4eaa90c54eeaa4b454
Salim Kachemela <sakachemela@gmail.com>
2026-04-06 22:37:03 +0300

    Change configuration for lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,18 +8,18 @@ return {
 		options = {
 			theme = "catppuccin", -- lualine theme
 		},
-	},
-	sections = {
-		lualine_c = {
-			{
-				"filename",
-				file_status = true,
-				newfile_status = false,
-				path = 4, -- 0: Just filename, 1: Relative path, 2: Absolute Path 3: Absolute Path with tilde, 4: Filename and parent dir, with tilde.
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					newfile_status = false,
+					path = 4, -- 0: Just filename, 1: Relative path, 2: Absolute Path 3: Absolute Path with tilde, 4: Filename and parent dir, with tilde.
 
-				symbols = {
-					modified = "[+]",
-					readonly = "[-]",
+					symbols = {
+						modified = "[+]",
+						readonly = "[-]",
+					},
 				},
 			},
 		},
